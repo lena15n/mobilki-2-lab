cdf2b820a30d12e93d9acb4b847480abdfe249e7
Nazarova Elena <ln15ln@rambler.ru>
2016-11-17 22:14:10 +0400

    Move logic to MeetingManager class, add @Singleton to restrict recreation of meetingsManager (it gave empty values earlier)

--- RESTful service/RESTful-service/src/restservice/MeetingsManager.java
+++ RESTful service/RESTful-service/src/restservice/MeetingsManager.java
@@ -1,11 +1,14 @@
 package restservice;
 
+import com.sun.jersey.spi.resource.Singleton;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
 import java.util.ArrayList;
 import java.util.Date;
+
 
 public class MeetingsManager {
     private static ArrayList<Meeting> meetings;
@@ -18,25 +21,25 @@ public class MeetingsManager {
         meetings = newMeetings;
     }
 
-    public static void addMeeting(){
+    public void addMeeting(){
         ArrayList<String> participants = new ArrayList<>();
         participants.add("part1");
         participants.add("part2");
         meetings.add(new Meeting("meet", "new meet", new Date(), new Date(), participants, "HIGHT"));
     }
 
-    public static void addMeeting(String name){
+    public void addMeeting(String name){
         ArrayList<String> participants = new ArrayList<>();
         participants.add("part1");
         participants.add("part2");
         meetings.add(new Meeting(name, "new meet", new Date(), new Date(), participants, "HIGHT"));
     }
 
-    public static void editMeeting(Meeting newMeeting){
+    public void editMeeting(Meeting newMeeting){
 
     }
 
-    public static void deleteMeeting(Meeting meeting){
+    public void deleteMeeting(Meeting meeting){
 
     }
 
--- RESTful service/RESTful-service/src/restservice/Service.java
+++ RESTful service/RESTful-service/src/restservice/Service.java
@@ -2,31 +2,36 @@ package restservice;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sun.jersey.spi.inject.Inject;
+import com.sun.jersey.spi.resource.Singleton;
 
 import javax.ws.rs.*;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
 import java.util.ArrayList;
 
+@Singleton
 @Path("/meetings")
 public class Service {
+   // @Context
+   // @Inject
     MeetingsManager meetingsManager;
 
-    public Service(){
+    public Service() {
         meetingsManager = new MeetingsManager();
     }
 
     @GET
     @Produces("application/json")
-    public Response getMeetings(){
+    public Response getMeetings() {
         ArrayList<Meeting> meetings = meetingsManager.getMeetings();
         ObjectMapper mapper = new ObjectMapper();
         String result;
         try {
             if (meetings.size() > 0) {
                 result = mapper.writeValueAsString(meetings);
-            }
-            else {
-              result = "empty now";
+            } else {
+                result = "empty now";
             }
 
         } catch (JsonProcessingException e) {
@@ -48,13 +53,10 @@ public class Service {
     @Path("/{param}")
     public Response putMeeting(@PathParam("param") String name) {
         //http://localhost:8080/sampel-glassfish-0.0.1-SNAPSHOT/crunchify/meetings/azaza
-        addMeeting(name);
+        meetingsManager.addMeeting(name);
         String output = "PUT: Jersey say : " + name + "\n meeting has created";
         return Response.status(200).entity(output).build();
     }
-
-
-
 
 
 }
