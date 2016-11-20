8c4125a78b56417c2adc552a341ae7f3994e400c
Nazarova Elena <ln15ln@rambler.ru>
2016-11-21 02:12:28 +0400

    Create new activity for new meeting

---
+++ AndroidClient/AndroidREST/app/src/main/java/com/lena/androidrest/CreateMeetingActivity.java
@@ -0,0 +1,13 @@
+package com.lena.androidrest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CreateMeetingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_meeting);
+    }
+}
--- AndroidClient/AndroidREST/app/src/main/java/com/lena/androidrest/MainActivity.java
+++ AndroidClient/AndroidREST/app/src/main/java/com/lena/androidrest/MainActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +14,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button createMeetingButton = (Button) findViewById(R.id.create_meeting_button);
+        if (createMeetingButton != null) {
+            createMeetingButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(getApplicationContext(), CreateMeetingActivity.class);
+                    startActivity(intent);
+                }
+            });
+        }
     }
 
     @Override
